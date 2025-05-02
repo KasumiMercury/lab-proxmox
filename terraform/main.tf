021f2f87984f58a23967688d4c39464af1a4f1e3
KasumiMercury <naoki_shira@outlook.jp>
2025-05-02 22:00:31 +0900

    terraform proxmox provider

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc8"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = ""
+  pm_tls_insecure = true
+}
+
