aa33d015abe2a731ecbbd85788c4847c460af003
KasumiMercury <naoki_shira@outlook.jp>
2025-05-04 09:54:30 +0900

    separate proxmox provider setting

--- terraform/modules/proxmox_cloudinit/main.tf
+++ terraform/modules/proxmox_cloudinit/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "Telmate/proxmox"
-      version = "3.0.1-rc8"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url      = ""
-  pm_tls_insecure = true
-}
-
 resource "proxmox_vm_qemu" "cloudinit" {
   vmid = 100
   name = var.vm_name
