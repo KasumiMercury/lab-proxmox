ec3ae764b0bc8e1c63c2895a475e550dd93423e4
KasumiMercury <naoki_shira@outlook.jp>
2025-05-13 23:29:29 +0900

    fix

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,7 +1,11 @@
 terraform {
   required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc8"
+    }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.7.2"
     }
   }
