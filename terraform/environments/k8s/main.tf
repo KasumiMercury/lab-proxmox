4e922acf57fc1167ad68906bd4fab6aa6af90764
KasumiMercury <naoki_shira@outlook.jp>
2025-05-13 22:27:31 +0900

    fix password output

--- terraform/environments/k8s/main.tf
+++ terraform/environments/k8s/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.7.2"
     }
   }
@@ -9,24 +9,27 @@ terraform {
 
 module "proxmox_cloudinit" {
   for_each = var.virtual_machines
-  source = "../../modules/proxmox_cloudinit"
-  
-  vmid = each.value.vmid
-  vm_name = each.value.vm_name
-  template = var.template
-  target_node = each.value.target_node
-  ip_address = each.value.ip_address
-  gateway = each.value.gateway
-  network_bridge = each.value.network_bridge
-  network_tag = each.value.network_tag
+  source   = "../../modules/proxmox_cloudinit"
+
+  vmid              = each.value.vmid
+  vm_name           = each.value.vm_name
+  template          = var.template
+  target_node       = each.value.target_node
+  ip_address        = each.value.ip_address
+  gateway           = each.value.gateway
+  network_bridge    = each.value.network_bridge
+  network_tag       = each.value.network_tag
   cloudinit_storage = var.cloudinit_storage
-  username = each.value.username
-  password_length = each.value.password_length
-  ssh_key_path = each.value.ssh_key_path
+  username          = each.value.username
+  password_length   = each.value.password_length
+  ssh_key_path      = each.value.ssh_key_path
 }
 
-output "password" {
-  value = module.proxmox_cloudinit.password
-  description = "Generated password for the VM"
-  sensitive   = true
+output "vm_passwords" {
+  description = "A map of VM names to their generated passwords."
+  value = {
+    for vm_key, vm_instance in module.proxmox_cloudinit :
+    vm_key => vm_instance.password
+  }
+  sensitive   = true 
 }
