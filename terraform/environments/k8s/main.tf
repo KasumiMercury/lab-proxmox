06c5f23390b06516b36201bbbe561ededdc62bc4
KasumiMercury <naoki_shira@outlook.jp>
2025-05-15 19:45:43 +0900

    cloudflare r2 backend

--- terraform/environments/k8s/main.tf
+++ terraform/environments/k8s/main.tf
@@ -1,6 +1,22 @@
 terraform {
   required_version = ">= 1.11" 
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "proxmox/k8s/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    use_lockfile                = true
+  }
   required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4"
+    }
     proxmox = {
       source  = "Telmate/proxmox"
       version = "3.0.1-rc8"
@@ -10,6 +26,10 @@ terraform {
       version = "3.7.2"
     }
   }
+}
+
+provider "cloudflare" {
+  # token pulled from $CLOUDFLARE_API_TOKEN
 }
 
 locals {
