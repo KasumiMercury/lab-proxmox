a1db6abd1bd0180d8774bdce29c840f73ddd695c
KasumiMercury <naoki_shira@outlook.jp>
2025-05-24 15:29:10 +0900

    [WIP] plan workflow

--- .github/workflows/select_env.sh
+++ .github/workflows/select_env.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # all_environments: all environments to be selected from
 # provided as a JSON array string
 # example: '["dev", "staging", "prod"]'
@@ -18,17 +20,17 @@ target_env=()
 # Convert the JSON array to a bash array
 all_environments=$(echo "$all_environments" | jq -r '.[]')
 
-if [ $target == "all" ]; then
+if [ "$target" == "all" ]; then
   # If target is "all", select all environments
-  target_env=($all_environments)
+  target_env=("$all_environments")
 else
   # If target is not "all", check if it exists in the provided environments
   if [[ $all_environments =~ $target ]]; then
-    target_env=($target)
+    target_env=("$target")
   else
     echo "Target environment '$target' not found in the provided environments. Exiting."
     exit 1
   fi
 fi
 
-echo "Selected environments: ${target_env[@]}"
+printf "%s\n" "${target_env[@]}" | jq -R . | jq -s . | jq -c .
